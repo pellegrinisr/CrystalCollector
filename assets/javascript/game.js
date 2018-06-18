74133cdfd32a5df6a4bd7ca0f34d82b48ee82083
Stephen Pellegrini <spellegrini156@gmail.com>
2018-06-18 14:35:19 -0700

    essentially done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,6 +14,7 @@ $(document).ready(function() {
         userTotal : 0,
         numWins : 0,
         numLosses : 0,
+        gameOver : false,
     
         generateNumbers : function() {
             this.targetVal = Math.floor(Math.random() * 102) + 19;
@@ -23,31 +24,69 @@ $(document).ready(function() {
             this.yellowVal = Math.floor(Math.random() * 12) + 1;
         },
     
-        resetGame : function() {
-            userTotal = 0;
+        resetGame : function () {
+            this.userTotal = 0;
+            $('.user-score').text(this.userTotal);
             this.generateNumbers();
             $(".target-number").text(this.targetVal);
+            this.gameOver = false;
+            $('.game-over-alert').text('');
+            $('.gem').on('click', crystalCollector.clickFunction);
+        }, 
+
+        clickFunction : function() {
+            if ($(this).attr('id') === 'blue') {
+                crystalCollector.userTotal += crystalCollector.blueVal;
+                $('.user-score').text(crystalCollector.userTotal);
+            }
+            else if ($(this).attr('id') === 'diamond') {
+                crystalCollector.userTotal += crystalCollector.diamondVal;
+                $('.user-score').text(crystalCollector.userTotal);
+            }
+            else if ($(this).attr('id') === 'red') {
+                crystalCollector.userTotal += crystalCollector.redVal;
+                $('.user-score').text(crystalCollector.userTotal);
+            }
+            else if ($(this).attr('id') === 'yellow') {
+                crystalCollector.userTotal += crystalCollector.yellowVal;
+                $('.user-score').text(crystalCollector.userTotal);
+            }
+            crystalCollector.checkGameStatus();
+        },
+
+        checkGameStatus : function() {
+            $('.user-score').text(this.userTotal);
+            console.log(this.userTotal); 
+            if (this.targetVal === this.userTotal) {
+                
+                //debugger;
+               // alert('you win');
+                //this.resetGame();
+                this.numWins++;
+                $(".num-wins").text(this.numWins);
+                this.gameOver = true;
+                $('.game-over-alert').text('You win! Press any key to play again.');
+                $('.gem').off('click');
+            }
+            else if (this.userTotal > this.targetVal) {
+                //debugger;
+                //alert ('you lose');
+                //this.resetGame();
+                this.numLosses++;
+                $(".num-losses").text(this.numLosses);
+                this.gameOver = true;
+                $('.game-over-alert').text('You Lose! Press any key to play again.');
+                $('.gem').off('click');
+            }  
         }
     };
-    crystalCollector.resetGame();
     
-    //event handler for clicking the crystals
-    $('.gem').on('click', function(ev) {
-        if ($(this).attr('id') === 'blue') {
-            crystalCollector.userTotal += crystalCollector.blueVal;
-            $('.user-score').text(crystalCollector.userTotal);
-        }
-        else if ($(this).attr('id') === 'diamond') {
-            crystalCollector.userTotal += crystalCollector.diamondVal;
-            $('.user-score').text(crystalCollector.userTotal);
-        }
-        else if ($(this).attr('id') === 'red') {
-            crystalCollector.userTotal += crystalCollector.redVal;
-            $('.user-score').text(crystalCollector.userTotal);
-        }
-        else if ($(this).attr('id') === 'yellow') {
-            crystalCollector.userTotal += crystalCollector.yellowVal;
-            $('.user-score').text(crystalCollector.userTotal);
+    crystalCollector.resetGame();
+    $('.gem').on('click', crystalCollector.clickFunction);
+
+    $(document).keyup(function() {
+        if (crystalCollector.gameOver) {
+            crystalCollector.resetGame();
         }
     });
 });
